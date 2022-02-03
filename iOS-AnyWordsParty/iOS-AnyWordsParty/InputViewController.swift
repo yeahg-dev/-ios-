7fbe5e7743eba1072ca1e741c096c5ef6a3df8de
yeahg-dev <yeahg.dev@gmail.com>
2022-02-03 22:58:42 +0900

    feat: AppDelegate에 persistentContainer 생성, InputViewController에 persistentContainer reference 전달

--- iOS-AnyWordsParty/iOS-AnyWordsParty/InputViewController.swift
+++ iOS-AnyWordsParty/iOS-AnyWordsParty/InputViewController.swift
@@ -6,24 +6,32 @@
 //
 
 import UIKit
+import CoreData
 
 class InputViewController: UIViewController {
-
+    
+    var container: NSPersistentContainer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        self.container = appDelegate.persistentContainer
+        
+        guard container != nil else {
+            fatalError("This view needs a persistent container.")
+        }
+        // The persistent container is available.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
