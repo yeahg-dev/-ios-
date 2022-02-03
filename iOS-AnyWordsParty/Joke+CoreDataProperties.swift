ebe8577d864de7592ba001dc1009cf5a79b781a0
yeahg-dev <yeahg.dev@gmail.com>
2022-02-03 22:39:27 +0900

    feat: Joke Entity에 attribute 추가 및 properties파일에 반영

--- iOS-AnyWordsParty/Joke+CoreDataProperties.swift
+++ iOS-AnyWordsParty/Joke+CoreDataProperties.swift
@@ -15,6 +15,11 @@ extension Joke {
     @nonobjc public class func fetchRequest() -> NSFetchRequest<Joke> {
         return NSFetchRequest<Joke>(entityName: "Joke")
     }
+    
+    // Entity가 활용하는 프로퍼티
+    @NSManaged public var body: String?
+    @NSManaged public var category: String?
+    @NSManaged public var id: UUID?
 
 
 }
