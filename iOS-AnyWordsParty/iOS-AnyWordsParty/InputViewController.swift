fa8f12799fe19206b1b538c87183301bb2f33ca3
yeahg-dev <yeahg.dev@gmail.com>
2022-02-03 21:15:44 +0900

    feat: InputViewController 생성 및 UI 구성

---
+++ iOS-AnyWordsParty/iOS-AnyWordsParty/InputViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InputViewController.swift
+//  iOS-AnyWordsParty
+//
+//  Created by 1 on 2022/02/03.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
